6c16499d6976f183aae1e55a33960ceef3b3ca78
rmwak <r.manso.a@gmail.com>
2018-05-07 13:31:56 +0200

    added filter for 1 class

---
+++ cat_detector_webcam_1_class.py
@@ -0,0 +1,124 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+from collections import defaultdict
+from io import StringIO
+from PIL import Image
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import ops as utils_ops
+
+import argparse
+
+if tf.__version__ < '1.4.0':
+  raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
+
+
+def parseArguments():
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+    # Optional arguments
+    parser.add_argument("-d", "--device", help="video device to use, defaults to 0", type=int, default=0)
+    parser.add_argument("-W", "--width", help="width of the camera shown, defaults to 480", type=int, default=480)
+    parser.add_argument("-H", "--height", help="height of the camera shown, defaults to 360", type=int, default=360)
+    parser.add_argument("-c", "--class_id", help="class to filter, defaults to 1 (person)", type=int, default=1)
+    parser.add_argument("-s", "--score", help="score threshold to use, defaults to 0.5", type=float, default=0.5)
+    # Parse arguments
+    args = parser.parse_args()
+    return args
+
+
+def filter_class(classes, scores, b_boxes, class_filter, score_filter):
+    return_list = []
+    for i in range(classes.shape[0]):
+        if classes[i]==class_filter and scores[i]>score_filter:
+            return_list.append({'class':classes[i], 'score':scores[i], 'b_box':b_boxes[i]})
+    return(return_list)
+
+def filter_class_as_lists(classes, scores, b_boxes, class_filter, score_filter):
+    #import ipdb; ipdb.set_trace() 
+    return_classes = []
+    return_scores = []
+    return_b_boxes = []
+    for i in range(classes.shape[0]):
+        if classes[i]==class_filter and scores[i]>score_filter:
+            return_classes.append(classes[i])
+            return_scores.append(scores[i])
+            return_b_boxes.append(b_boxes[i])
+    return(return_classes,return_scores,return_b_boxes)
+
+
+def run_detection_webcam(device,width,height,class_id,score):
+
+    cap = cv2.VideoCapture(device)
+    # Running the tensorflow session
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+            ret = True
+            while (ret):
+                ret,image_np = cap.read()
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                scores = detection_graph.get_tensor_by_name('detection_scores:0')
+                classes = detection_graph.get_tensor_by_name('detection_classes:0')
+                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+                # Actual detection.
+                (boxes, scores, classes, num_detections) = sess.run(
+                    [boxes, scores, classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
+                classes,scores,boxes = filter_class_as_lists(np.squeeze(classes),
+                                                            np.squeeze(scores),
+                                                            np.squeeze(boxes),
+                                                            class_id,
+                                                            score)
+                boxes = np.array(boxes)
+                classes = np.array(classes).astype(np.int32)
+                num_detections = len(classes)
+                # Visualization of the results of a detection.
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    boxes,
+                    classes,
+                    scores,
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=8)
+                cv2.imshow('image',cv2.resize(image_np,(width,height)))
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    cap.release()
+                    break
+
+if __name__ == "__main__":
+
+    args = parseArguments()
+    print(args)
+    # Path to frozen detection graph. This is the actual model that is used for the object detection.
+    PATH_TO_CKPT = 'model/frozen_inference_graph.pb'
+    # List of the strings that is used to add correct label for each box.
+    PATH_TO_LABELS = 'data/mscoco_label_map.pbtxt'
+    #number of classes
+    NUM_CLASSES = 90
+
+    #load graph
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+    #load labels
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    run_detection_webcam(args.device,args.width,args.height,args.class_id,args.score)
